d4a6066a6def03413f1a49c0f44c2caecf820b71
BROGIER13 <43548740+BROGIER13@users.noreply.github.com>
2019-12-06 14:06:48 +0100

    Update table_des_symboles.h

--- LEPROJET/Modules/tables_des_symboles/table_des_symboles.h
+++ LEPROJET/Modules/tables_des_symboles/table_des_symboles.h
@@ -5,12 +5,10 @@
 
 
 typedef struct  {
-
     char  nom_label[MAX_LABEL_SIZE];
     int valeur;
     enum status {CODE , DATA , UNDEFINED};
     enum scope {LOCAL , GLOBAL , EXTERN};
-
 } symbol;
 
 typedef struct {
@@ -21,3 +19,6 @@ typedef struct {
 //model_symbol_table symbol_table; (à déclarer dans assembleur)
 
 int insert_symbol(t_instruction instruction);
+//function rechercher
+//function insert_with_value
+//function insert_no_value
