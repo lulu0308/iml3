355b1fa907cd745188251f6c4a370afb5b01def0
BROGIER13 <43548740+BROGIER13@users.noreply.github.com>
2019-12-27 16:39:05 +0100

    Update assembleur.c

--- LEPROJET/Modules/assembleur/assembleur.c
+++ LEPROJET/Modules/assembleur/assembleur.c
@@ -1,4 +1,60 @@
-function passe1(){}
+
+static int compteur;
+function passe1(t_asm_line line, t_asm_directive directive)
+{
+  while (line != '/0')
+  {
+    parse_line();
+    if (line != '#')
+    {
+      if (&line == line.label)
+      {
+        if (recherche(line.label))
+        {
+          printf("erreur double définition");
+        }else{
+          insert_symbol (line.label, inst_info1 table1 []);
+          compteur ++;
+        }
+      }
+      if (line.instruction_type == ASM_DIRECTIVE)
+      {
+        lg_data = ??;
+        compteur = compteur + lg_data;
+      }
+      if (!recherche(line.label))
+      {
+        insert.symbol(line.label, inst_info1 table1 []);
+      }
+      if (line.label = .globl)
+          if (!recherche(line.label))
+          {
+            insert_symbol (line.label, inst_info1 table1 []);
+          }else{
+            modif_table();
+          }
+      }
+      if (line.instruction_type == NONE)
+      {
+        compteur == 0;
+        /* entrer le nom section dans TS*/
+      }
+      if (line.instruction_type == ASM_INSTRUCTION)
+      {
+        if (valide)
+        {
+        lg_instr = ??;
+        compteur = compteur + lg_instr;
+        }else{
+          printf("erreur instruction invalide");
+        }
+      }
+    }
+  }
+}
+
+          
+          
 function passe2(){}
 
 static model_symbol_table symbol_table;
