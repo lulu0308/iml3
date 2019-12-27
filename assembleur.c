1af131602114526648d11b75a8a9efb480b8be3b
BROGIER13 <43548740+BROGIER13@users.noreply.github.com>
2019-12-27 13:48:38 +0100

    Update assembleur.c

--- assembleur.c
+++ assembleur.c
@@ -1,1 +1,13 @@
 #include "assembleur.h"
+
+
+static int compteur;
+funct passe1_ass (t_asm_line line)
+{
+  while (line != '/0')
+  {
+    //read ligne
+    if (line != '#')
+    {
+      if (line 
+      
