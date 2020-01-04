ada2b3639088506ea035e40d8345b9f1d2e1d17c
BROGIER13 <43548740+BROGIER13@users.noreply.github.com>
2020-01-04 17:52:44 +0100

    Update table_des_symboles.c

--- LEPROJET/Modules/HEADERS/table_des_symboles.c
+++ LEPROJET/Modules/HEADERS/table_des_symboles.c
@@ -4,3 +4,26 @@
 void test(){
     printf("HelloWorld");
 }
+
+static int i;
+int insert_symbol(t_asm_line line, model_symbol_table symtable)
+{
+	symtable.table[i].label = line.label;
+	symtable.table[i].valeur = line.inst.operand3;//.value
+	symtable.table[i].status = line.instruction_type;
+	symtable.table[i].scope = line. ??
+	i++;
+}
+
+
+int recherche(t_asm_line line, model_symbol_table symtable)
+{
+	for (int i = 0; i < effective_size; i++)
+	{
+		if(line == symtable.table[i])
+		{
+			return true;
+		}else{
+			return false;
+		}
+}
