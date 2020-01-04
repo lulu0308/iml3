c3b0f7a19e3685489dcd9e9b7702afa8678c8019
BROGIER13 <43548740+BROGIER13@users.noreply.github.com>
2020-01-04 17:54:28 +0100

    Update assembleur.c

--- LEPROJET/Modules/assembleur.c
+++ LEPROJET/Modules/assembleur.c
@@ -104,3 +104,92 @@ int main(int argc, char **argv)
 		return (0);
 	}
 }
+
+
+static int compteur1;
+function passe1(t_asm_line line, t_asm_directive directive)
+{
+  while (!fin-de-fichier())
+  {
+    parse_line();
+    if (line != '#')
+    {
+      if (&line == line.label)
+      {
+        if (recherche(line.label))
+        {
+          printf("erreur double définition");
+        }else{
+          insert_symbol (line.label, inst_info1 table1 []);
+          compteur1 ++;
+        }
+      }
+      if (line.instruction_type == ASM_DIRECTIVE)
+      {
+        lg_data = line.directive.int_operands;
+        compteur1 = compteur1 + lg_data;
+      }
+      if (!recherche(line.label))
+      {
+        insert.symbol(line.label, inst_info1 table1 []);
+      }
+      if (line.label = .globl)
+          if (!recherche(line.label))
+          {
+            insert_symbol (line.label, inst_info1 table1 []);
+          }else{
+            modif_table();
+          }
+      }
+      if (line.instruction_type == NONE)
+      {
+        compteur1 == 0;
+        /* entrer le nom section dans TS*/
+      }
+      if (line.instruction_type == ASM_INSTRUCTION)
+      {
+        if (valide)
+        {
+        lg_instr = 4;
+        compteur1 = compteur1 + lg_instr;
+        }else{
+          printf("erreur instruction invalide");
+        }
+      }
+    }
+  }
+}
+
+
+static int compteur2;  
+function passe2(){
+  while (!fin-de-fichier())
+  {
+    parse_line();
+    if (line != '#')
+    {
+      if (line.instruction_type == NONE)
+      {
+        compteur1 == 0;
+        /* mettre à jour l'entête*/
+      }
+      if (line.instruction_type == ASM_INSTRUCTION)
+      {
+        lg_instr = 4;
+        //assembler instr
+        //ecrire code instr
+        compteur2 = compteur2 + lg_instr;
+      }
+      if (line.instruction_type == ASM_DIRECTIVE)
+      {
+        lg_data = line.directive.int_operands;
+        compteur2 = compteur2 + lg_data;
+        //traiter directive
+      }
+    }
+  }
+  //maj entete
+  //ecrire tgre
+  //ecrire autres infos
+}
+   
