f6c8bc1fd62c170bd96f9fac01a4942e240646a0
lulu0308 <58552172+lulu0308@users.noreply.github.com>
2020-01-10 10:03:18 +0100

    Update table_des_symboles.h

--- LEPROJET/Modules/tables_des_symboles/table_des_symboles.h
+++ LEPROJET/Modules/tables_des_symboles/table_des_symboles.h
@@ -16,9 +16,17 @@ typedef struct {
     symbol table[MAX_SYMBOL_COUNT][MAX_SYMBOL_COUNT];
 } model_symbol_table;
 
-//model_symbol_table symbol_table; (à déclarer dans assembleur)
-
-int insert_symbol(t_instruction instruction);
-//function rechercher
-//function insert_with_value
-//function insert_no_value
+char *get_opcode(int opcode_id);
+int is_pseudo(t_asm_line k);
+//void write_pseudo(t_asm_line *line, FILE *file);
+//void print_t_operand(t_operand x);
+//void print_t_instruction(t_instruction x);
+//void print_t_directive(t_directive x);
+int size_of_data(t_directive x, int compteur);
+//void print_t_asm_line(t_asm_line *x);
+int get_size_of_pseudo(int x);
+void print_symboltable();
+int recherche(char *string);
+int recherche_and_have_value(char *string);
+void insert_symbol(char *string, int compteur, int scope, int status);
+void premier_passe(t_asm_line *line);
